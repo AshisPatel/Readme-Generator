9fdba307eac9cecb59a96acf6e39e6c0f29e7ac0
AP <ashis.n.patel@gmail.com>
2021-08-15 17:00:32 -0500

    Add prompt for user to include install / test instructions and make a list

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const questions = [];
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-const getProjectDetails = () => {
+const getReadmeDetails = () => {
     return inquirer
         .prompt([
             {
@@ -31,32 +31,110 @@ const getProjectDetails = () => {
                 message: 'Enter a description for the project: '
             },
             {
-                // Break code here to enter a new prompt to generate an array for steps for installation... 
-                type: 'input',
-                name: 'installation',
-                message: 'Enter installation instructions for the project:'
-            },
-            {
                 type: 'input',
                 name: 'usage',
-                message: 'Enter usage information for the project:'
+                message: 'Enter usage information for the project: '
             },
             {
                 type: 'input',
                 name: 'contributions',
-                message: 'Enter contribution guidelines'
+                message: 'Enter contribution guidelines: '
             },
             {
-                // Break code here to enter a new prompt to generate an array for testing steps... 
-                type: 'input',
-                name: 'test',
-                message: 'Enter test instructions'
+                type: 'confirm',
+                name: 'installInstructionsCheck', 
+                message: 'Will this Readme include installation instructions?', 
+                default: false 
+            },
+            {
+                type: 'confirm',
+                name: 'testInstructionsCheck',
+                message: 'Will this Readme include test instructions?',
+                default: false
             }
 
         ])
 }
 
+ const promptInstallInstructions = readmeData => {
+    // Check if user wants to add install instructions, if not skip to next prompt 
+    if (!readmeData.installInstructionsCheck) {
+        return readmeData; 
+    }
+    // Create an array to hold install instructions 
+    if(!readmeData.install) {
+        readmeData.install = []; 
+    }
+     // Print array of current steps, if the array is not empty
+    //  if(readmeData.install.length != 0 ) {
+    //     console.log(`Current Instructions: ${readmeData.install}`);
+    //  }
+    
+    return inquirer.prompt([
+        {
+            type: 'input', 
+            name: 'instruction',
+            message: 'Enter installation instruction step:' 
+        },
+        {
+            type: 'confirm',
+            name: 'confirmAddInstructions',
+            message: 'Would you like to add more instructions?', 
+            default: false
+        }
+    ]).then(instructionsData => {
+        readmeData.install.push(instructionsData); 
+        if (instructionsData.confirmAddInstructions) {
+            return promptInstallInstructions(readmeData); 
+        } else {
+            return readmeData; 
+        }
+    })
+ }
+
+ const promptTestInstructions = readmeData => {
+    //  Check if user wants to add test instructions
+    if(!readmeData.testInstructionsCheck) {
+        return readmeData; 
+    }
+
+    // Create an array to hold test instructions 
+    if(!readmeData.test) {
+        readmeData.test = []; 
+    }
+
+    // Print array of current steps, if the array is not empty
+    // if (readmeData.test.length != 0) {
+    //     console.log(`Current Instructions: ${readmeData.test}`);
+    // }
+   
+
+    return inquirer.prompt([
+        {
+            type: 'input', 
+            name: 'instruction',
+            message: 'Enter test instruction step:' 
+        },
+        {
+            type: 'confirm',
+            name: 'confirmAddInstruction',
+            message: 'Would you like to add more instructions?', 
+            default: false
+        }
+    ]).then(instructionsData => {
+        readmeData.test.push(instructionsData); 
+        if (instructionsData.confirmAddInstruction) {
+            return promptTestInstructions(readmeData); 
+        } else {
+            return readmeData; 
+        }
+    })
+ }
+
 
 // Function call to initialize app
-getProjectDetails().then(projectData => console.log(projectData)); 
+getReadmeDetails()
+    .then(readmeData => promptInstallInstructions(readmeData))
+    .then(readmeData => promptTestInstructions(readmeData))
+    .then(readmeData => console.log(readmeData));  
 
